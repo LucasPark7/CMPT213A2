0d070fd1efebf0129e6b4daec8c90de7dffe8146
LucasPark7 <14225911+LucasPark7@users.noreply.github.com>
2020-06-26 12:28:52 -0700

    Added to Hero class

--- src/cmpt213a2/model/Hero.java
+++ src/cmpt213a2/model/Hero.java
@@ -6,11 +6,13 @@ public class Hero {
      private int numPowers;
      private int yCoord;
      private int xCoord;
+     private int monstersRemain;
 
-    public Hero(int numPowers, int yCoord, int xCoord) {
+    public Hero(int numPowers, int yCoord, int xCoord, int monstersRemain) {
         this.numPowers = numPowers;
         this.yCoord = yCoord;
         this.xCoord = xCoord;
+        this.monstersRemain = monstersRemain;
     }
 
     public int getNumPowers() {
@@ -35,6 +37,14 @@ public class Hero {
 
     public void setxCoord(int xCoord) {
         this.xCoord = xCoord;
+    }
+
+    public int getMonstersRemain() {
+        return monstersRemain;
+    }
+
+    public void setMonstersRemain(int monstersRemain) {
+        this.monstersRemain = monstersRemain;
     }
 
     private void moveHero(String input) {
